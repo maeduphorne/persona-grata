f15921cdafda118791c0007b75708544558cb6f7
Mae Duphorne <maelduphorne@gmail.com>
2021-09-22 20:35:51 -0600

    add shell for suggested changes

--- src/Components/SuggestedQuestions/SuggestedQuestions.js
+++ src/Components/SuggestedQuestions/SuggestedQuestions.js
@@ -1,7 +1,16 @@
 import './SuggestedQuestions.css'
 
 const SuggestedQuestions = () => {
-
+    return (
+        <div>
+            <h2>
+                Other questions to ask when considering a user persona?
+            </h2>
+            <ul>
+                <li></li>
+            </ul>
+        </div>
+    )
 }
 
 export default SuggestedQuestions;
