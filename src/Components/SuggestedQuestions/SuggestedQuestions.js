61b9a1f7c435edc0959a9cb3bf54bf1ce953cf88
Mae Duphorne <maelduphorne@gmail.com>
2021-09-22 20:59:03 -0600

    Write user persona questions

--- src/Components/SuggestedQuestions/SuggestedQuestions.js
+++ src/Components/SuggestedQuestions/SuggestedQuestions.js
@@ -4,10 +4,17 @@ const SuggestedQuestions = () => {
     return (
         <div>
             <h2>
-                Other questions to ask when considering a user persona?
+                Questions to ask about your new user:
             </h2>
             <ul>
-                <li></li>
+                <li>What does this user do for a living?</li>
+                <li>What is the user's personality like?</li>
+                <li>What are their likes & dislikes?</li>
+                <li>What kind of products does this person use?</li>
+                <li>What might their needs be?</li>
+                <li>What might their frustrations be?</li>
+                <li>Are they tech saavy?</li>
+                <li>What are their habits surrounding technology?</li>
             </ul>
         </div>
     )
