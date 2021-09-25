7d63cd662060f81a5e49159a5b007ee57553ac1b
Mae Duphorne <maelduphorne@gmail.com>
2021-09-24 18:42:24 -0600

    attempt bug fix for userinfo quote combo

--- src/Components/SavedUsers/SavedUsers.js
+++ src/Components/SavedUsers/SavedUsers.js
@@ -1,13 +1,21 @@
 import './SavedUsers.css'
+import UserCard from '../UserCard/UserCard';
 
 const SavedUsers = ({ savedUsersInfo, savedUsersQuotes}) => {
 //bring in savedUsers from App as a prop
 //array of saved users
-  return(
-    <div>
-      Saved Users here!
-    </div>
-  )
+
+  const savedUserCards = savedUsersInfo.map((userInfo) => {
+    return(
+      <div>
+        <UserCard 
+          userInfo={userInfo} 
+          userQuote={UserQuote}
+        />
+      </div>
+    )
+  })
+  
 }
 
 export default SavedUsers;
