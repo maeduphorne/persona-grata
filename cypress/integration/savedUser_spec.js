c8845cb7bf06a7dd38136ffd31f3038342244e0e
Mae Duphorne <maelduphorne@gmail.com>
2021-09-25 18:17:20 -0600

    add shell of savedUser test and beforeEach

--- cypress/integration/savedUser_spec.js
+++ cypress/integration/savedUser_spec.js
@@ -1,8 +1,32 @@
 describe('Saved User page render', () => {
   beforeEach(() => {
     cy.visit('http://localhost:3000/')
-    cy.get()
+    cy.wait(500)
+    cy.get('.save-user-btn').click()
+    cy.get('.generate-user-btn').click()
+    cy.wait(500)
+    cy.get(".save-user-btn").click()
+    cy.get('.saved-link').click()
   });
 
-  it()
+  it('Should navigate to Saved User page', () => {
+    cy.url().should('eq', 'http://localhost:3000/savedUsers')
+      .url().should('not.eq', 'http://localhost:3000/')
+  })
+
+  it('Should display a set of suggested questions on load', () => {
+    
+  })
+
+  it('Should display multiple user cards', () => {
+
+  })
+
+  it('Each card should contain the user\'s information', () => {
+    
+  })
+
+  it('Should have a button to navigate back to the home page', () => {
+    
+  })
 })
