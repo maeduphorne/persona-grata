c0bddb5e557b61bd47b0a9566ad08720071b73ff
Mae Duphorne <maelduphorne@gmail.com>
2021-09-26 12:14:41 -0600

    give li classnames

--- src/Components/SuggestedQuestions/SuggestedQuestions.js
+++ src/Components/SuggestedQuestions/SuggestedQuestions.js
@@ -7,14 +7,14 @@ const SuggestedQuestions = () => {
                 Questions to ask about your new user:
             </h3>
             <ul className="questions-list">
-                <li>What does this user do for a living?</li>
-                <li>What is the user's personality like?</li>
-                <li>What are their likes & dislikes?</li>
-                <li>What kind of products does this person use?</li>
-                <li>What might their needs be?</li>
-                <li>What might their frustrations be?</li>
-                <li>Are they tech saavy?</li>
-                <li>What are their habits surrounding technology?</li>
+                <li className='q1'>What does this user do for a living?</li>
+                <li className='q2'>What is the user's personality like?</li>
+                <li className='q3'>What are their likes & dislikes?</li>
+                <li className='q4'>What kind of products does this person use?</li>
+                <li className='q5'>What might their needs be?</li>
+                <li className='q6'>What might their frustrations be?</li>
+                <li className='q7'>Are they tech saavy?</li>
+                <li className='q8'>What are their habits surrounding technology?</li>
             </ul>
         </aside>
     )
