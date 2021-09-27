fd539e49439ecf3171add2d6ffc86072099c7c3e
Mae Duphorne <maelduphorne@gmail.com>
2021-09-27 16:49:10 -0600

    add fetch error for random user component

--- src/Components/RandomUser/RandomUser.js
+++ src/Components/RandomUser/RandomUser.js
@@ -6,7 +6,7 @@ import UserCard from '../UserCard/UserCard';
 const RandomUser = ({ savedUsersInfo, setSavedUsersInfo }) => {
   const [userInfo, setUserInfo] = useState({});
   const [btnIsToggled, setbtnIsToggled] = useState(false);
-  // const [userInfoError, setUserInfoError] = useState('')
+  const [userInfoError, setUserInfoError] = useState('')
 
   const fetchUser = () => {
     return Promise.all([apiCalls.fetchRandomUser(), apiCalls.fetchRandomQuote()])
@@ -33,7 +33,7 @@ const RandomUser = ({ savedUsersInfo, setSavedUsersInfo }) => {
       data.author = promises[1].author
       setUserInfo(data)
     })
-    // .catch(error => setUserInfoError('Unable to find a user. Please refresh the page or try again later.'))
+    .catch(error => setUserInfoError('Unable to find a user. Please refresh the page or try again later.'))
 }
 //   const getRandomUser = () => {
 //     apiCalls.fetchRandomUser()
@@ -90,16 +90,16 @@ return (
         Read their randomly generated details carefully and let your imagination run wild!<br></br>
         Refer to the Suggested Questions section for assistance in thinking through your user's personality.
       </h2>
-      {/* { userInfo && */}
-      <section className="random-user-section">
-        <UserCard userInfo={userInfo} />
-        <div className="button-wrapper">
-          <button className="generate-user-btn" onClick={handleNewUser}>Generate New User</button>
-          <button className="save-user-btn" onClick={handleSavedUsersClick}>Save User Persona</button>
-        </div>
-      </section>
-      {/* } */}
-      {/* {!userInfo && userInfoError && <section className="user-error"> {userInfoError} </section>} */}
+      {!userInfo.length && userInfoError && <section className="user-error"> {userInfoError} </section>}
+      { userInfo &&
+        <section className="random-user-section">
+          <UserCard userInfo={userInfo} />
+          <div className="button-wrapper">
+            <button className="generate-user-btn" onClick={handleNewUser}>Generate New User</button>
+            <button className="save-user-btn" onClick={handleSavedUsersClick}>Save User Persona</button>
+          </div>
+        </section>
+      }
   </div>
 )
 }
