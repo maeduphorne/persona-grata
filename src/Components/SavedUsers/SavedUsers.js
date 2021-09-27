84d6e3a869d6eefb322d0ec7ab4cad19b4139c42
Mae Duphorne <maelduphorne@gmail.com>
2021-09-27 16:43:45 -0600

    change sugg questions to p tags

--- src/Components/SavedUsers/SavedUsers.js
+++ src/Components/SavedUsers/SavedUsers.js
@@ -15,7 +15,6 @@ const SavedUsers = ({ savedUsersInfo }) => {
       {!savedUsersInfo.length && <h3 className="no-saved">You have not saved any users yet! Please return home to generate a random user</h3>}
       {savedUserCards}
     </div>
-    //add error message if there are no saved users
   )
 }
 
--- src/Components/SuggestedQuestions/SuggestedQuestions.js
+++ src/Components/SuggestedQuestions/SuggestedQuestions.js
@@ -6,16 +6,16 @@ const SuggestedQuestions = () => {
             <h3 className="questions-header">
                 Questions to ask about your new user:
             </h3>
-            <ul className="questions-list">
-                <li className='q1'>What does this user do for a living?</li>
-                <li className='q2'>What is the user's personality like?</li>
-                <li className='q3'>What are their likes & dislikes?</li>
-                <li className='q4'>What kind of products does this person use?</li>
-                <li className='q5'>What might their needs be?</li>
-                <li className='q6'>What might their frustrations be?</li>
-                <li className='q7'>Are they tech saavy?</li>
-                <li className='q8'>What are their habits surrounding technology?</li>
-            </ul>
+            <section className="questions-list">
+                <p className='q1'>What does this user do for a living?</p>
+                <p className='q2'>What is the user's personality like?</p>
+                <p className='q3'>What are their likes & dislikes?</p>
+                <p className='q4'>What kind of products does this person use?</p>
+                <p className='q5'>What might their needs be?</p>
+                <p className='q6'>What might their frustrations be?</p>
+                <p className='q7'>Are they tech saavy?</p>
+                <p className='q8'>What are their habits surrounding technology?</p>
+            </section>
         </aside>
     )
 }
